d537b6e9390385d655a3418bfafb4560b97540b7
Larry <lp.apicella@gmail.com>
2019-12-14 12:14:02 +0100

    Tic-Tac-Toe second stage

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -1,5 +1,41 @@
 package tictactoe;
+import java.util.Scanner;
 
+public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    private static String[][] grid = new String[3][3];
+    public static void main(String[] args) {
+        fillGrid();
+        showGrid();
+    }
+
+    private static void fillGrid(){
+        String inputLine = sc.nextLine();
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                int beg = 3*i + j;
+                grid[i][j] = inputLine.substring(beg, beg+1);
+            }
+        }
+
+
+    }
+
+    private static void showGrid(){
+        System.out.println("---------");
+        for(int i=0; i<3; i++){
+            System.out.print("| ");
+            for(int j=0; j<3; j++){
+                System.out.print(grid[i][j]+" ");
+            }
+            System.out.print("|\n");
+        }
+        System.out.println("---------");
+    }
+}
+
+/* STAGE 1
+package tictactoe;
 import java.util.Random;
 
 public class Main {
@@ -27,3 +63,4 @@ public class Main {
         }
     }
 }
+ */
